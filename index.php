97f405598ee5a196aa77b9c01d0632a11933646b
Andi Moh Soreang <85069651+andimohsoreang@users.noreply.github.com>
2021-06-01 00:00:31 +0800

    Membuat File Index.php untuk landing Page
    Membuat File index untuk tampilan landing page web apps.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyHotel</title>
+</head>
+<body>
+    
+    <h1>Welcome To My Hotel</h1>
+
+    <h3>We Ready To serve you better :)</h3>
+    <ul>
+        <li> <a href="form_orders.php">Get Your Room !</a> </li>
+    </ul>
+
+</body>
+</html>
